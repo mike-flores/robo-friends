02e9cb924b082f48d44732c8f7cfd72cdf17246c
mike-flores <flo1985@gmail.com>
2019-03-18 18:38:39 -0700

    refactored some code in app to make app the parent component that passes state between cardlist and searchBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import CardList from './components/card/CardList';
+import { robots } from './components/card/robots'
 
 class App extends React.Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         robots: robots
+      }
+   }
+
    render() {
       return (
          <div className='tc'>
             <h1>RoboFriends</h1>
-            <CardList />
+            <CardList robots={this.state.robots} />
          </div>
       );
    }
--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import Card from './Card';
-import { robots } from './robots';
 
-class CardList extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         robots: robots
-      };
-   }
-   render() {
+
+const CardList = props => {
+   
       return (
          <div>
-            {this.state.robots.map(robot => {
+            {props.robots.map(robot => {
                return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />;
             })}
          </div>
       );
-   }
-   componentDidMount() {
-      console.log(this.state.robots);
-   }
+ 
 }
 export default CardList;
